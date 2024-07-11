ea39c2b3d901cd01d5397f31c786c62137eb6619
nghianek123 <2051052089nghia@ou.edu.vn>
2024-07-12 00:34:52 +0700

    minimal api asp.net

---
+++ testapi/testapi/Todo.cs
@@ -0,0 +1,9 @@
+﻿namespace testapi
+{
+    public class todo
+    {
+        public int id { get; set; }
+        public string? name { get; set; }
+        public bool iscomplete { get; set; }
+    }
+}
